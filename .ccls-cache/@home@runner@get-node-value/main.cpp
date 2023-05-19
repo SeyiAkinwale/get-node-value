1594899ec0e070851b5baf5df42bce91c42ab1c8
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-19 16:47:46 +0000

    first test

--- .ccls-cache/@home@runner@get-node-value/main.cpp
+++ .ccls-cache/@home@runner@get-node-value/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+#include <string>
+
+class Node {
+  public:
+    std::string val;
+    Node* next;
+
+    Node(std::string initialVal) {
+      val = initialVal;
+      next = nullptr;
+    }
+};
+
+
+
+std::string getNodeValue(Node* head, int index) 
+{
+  Node * current = head;
+  int location=0;
+  while(current!=nullptr)
+  {
+    if(location==index) return current->val;
+    location++; current=current->next;
+  }
+  return "";
+}
 
 int main() {
-  std::cout << "Hello World!\n";
+  Node a("a");
+Node b("b");
+Node c("c");
+Node d("d");
+
+a.next = &b;
+b.next = &c;
+c.next = &d;
+
+// a -> b -> c -> d
+
+std::cout<<getNodeValue(&a, 2); // "c"
 }
